1097be7033923a23d5069a7212ee1fa3ff92b766
kimmeh <mhko0411@gmail.com>
2022-09-13 23:07:56 +0900

    scoll 이벤트를 활용한 무한스크롤 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { container } from "./projects/InfiniteScroll";
+import { scrollContainer } from "./projects/InfiniteScroll/scroll";
 
 window.onload = () => {
   const root = document.querySelector("#root");
 
-  root.appendChild(container);
+  root.appendChild(scrollContainer);
 };
--- src/projects/InfiniteScroll/scroll.js
+++ src/projects/InfiniteScroll/scroll.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+import { createBox } from "./utils";
+
+let count = 0;
+
+const scrollContainer = document.createElement("div");
+scrollContainer.className = "container";
+
+createBox(scrollContainer, 10, 0);
+
+document.addEventListener("scroll", (e) => {
+  const scrollY = window.scrollY;
+  const innerHeight = window.innerHeight;
+  const bodyHeight = document.body.offsetHeight;
+
+  if (scrollY + innerHeight >= bodyHeight) {
+    createBox(scrollContainer, 10, count);
+    count += 1;
+  }
+});
+
+export { scrollContainer };
